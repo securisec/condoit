77ab21e951745c37bda01a4522d6a5816cc349ae
securisec <securisecctf@gmail.com>
2020-01-03 20:50:33 -0500

    ✅ 📚
    📚 updated docs
    ✅ maniphest methods done

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Condoit
 
-Condoit is a JS/TS Node library compitable for the [Phabricator Conduit](https://secure.phabricator.com/conduit/method/) api. All the methods available are named using the same convention as the Conduit endpoints with the sole difference being camel cased instead of using the `.`. The library does offer intellisense for most methods due to development being done in TS. 
+Condoit is a JS/TS Node library compitable for the [Phabricator Conduit](https://secure.phabricator.com/conduit/method/) api. All the methods available are named using the same convention as the Conduit endpoints. The library does offer intellisense for most methods. 
 
 This library is being built around Phabricator commit hash `54bcbdaba94a3573e128c6498816dbfa41d3a9cb` pushed on Dec 13th 2019. This library includes most of the frozen methods found here, and future versions of Condoit may deprecate those methods if Phabricator depricates them.
 
@@ -17,7 +17,7 @@ All the methods returns a promise.
 const { Condoit } = require('condoit')
 
 let condoit = new Condoit('https://path/to/phabricator', 'api-token')
-condoit.userWhoami().then(data => console.log(data))
+condoit.user.whoami().then(data => console.log(data))
 ```
 
 ### TS
@@ -25,5 +25,5 @@ condoit.userWhoami().then(data => console.log(data))
 import { Condoit } from 'condoit'
 
 let condoit = new Condoit('https://path/to/phabricator', 'api-token')
-condoit.userWhoami().then(data => console.log(data))
+condoit.user.whoami().then(data => console.log(data))
 ```
--- src/index.ts
+++ src/index.ts
@@ -365,8 +365,18 @@ export class Condoit {
 			// TODO
 		},
 
-		info: () => {
-			// TODO
+		/**
+		 ***Marked for deprecation** Get information about a file. 
+		 [Docs]{@link https://secure.phabricator.com/conduit/method/file.info/}
+		 *
+		 * @param {{ id?: number; phid?: string }} options
+		 * @returns {Promise<object>}
+		 */
+		info: (options: { id?: number; phid?: string }): Promise<object> => {
+			return this.makeRequest('file.info', {
+				id: options?.id,
+				phid: options?.phid
+			});
 		},
 
 		querychunks: () => {
@@ -456,17 +466,141 @@ export class Condoit {
 		}
 	};
 
-	public manifest = {
-		createtask: () => {
-			// TODO
+	public maniphest = {
+		/**
+		 ***Marked for deprecation. 
+		 [Docs]{@link https://secure.phabricator.com/conduit/method/maniphest.createtask/}
+		 *
+		 * @param {{
+		 * 			title: string;
+		 * 			description?: string;
+		 * 			ownerPHID?: string;
+		 * 			viewPolicy?: string;
+		 * 			editPolicy?: string;
+		 * 			ccPHIDs?: Array<string>;
+		 * 			priority?: number;
+		 * 			projectPHIDs?: Array<string>;
+		 * 			auxiliary: object;
+		 * 		}} options Only title is required. All other options are optional. 
+		 * @returns {Promise<object>}
+		 */
+		createtask: (options: {
+			title: string;
+			description?: string;
+			ownerPHID?: string;
+			viewPolicy?: string;
+			editPolicy?: string;
+			ccPHIDs?: Array<string>;
+			priority?: number;
+			projectPHIDs?: Array<string>;
+			auxiliary: object;
+		}): Promise<object> => {
+			return this.makeRequest('maniphest.createtask', {
+				title: options?.title,
+				description: options?.description,
+				ownerPHID: options?.ownerPHID,
+				viewPolicy: options?.viewPolicy,
+				ccPHIDs: options?.ccPHIDs,
+				priority: options?.priority,
+				projectPHIDs: options?.projectPHIDs,
+				auxiliary: options?.auxiliary
+			});
 		},
 
-		edit: () => {
-			// TODO
+		/**
+		 *Edit a maniphest task. 
+		 [Docs]{@link https://secure.phabricator.com/conduit/method/maniphest.edit/}
+		 *
+		 * @param {([
+		 * 				{
+		 * 					type:
+		 * 						| 'parent'
+		 * 						| 'column'
+		 * 						| 'space'
+		 * 						| 'title'
+		 * 						| 'owner'
+		 * 						| 'status'
+		 * 						| 'priority'
+		 * 						| 'description'
+		 * 						| 'parents.add'
+		 * 						| 'parents.remove'
+		 * 						| 'parents.set'
+		 * 						| 'subtasks.add'
+		 * 						| 'subtasks.remove'
+		 * 						| 'subtasks.set'
+		 * 						| 'commits.add'
+		 * 						| 'commits.remove'
+		 * 						| 'commits.set'
+		 * 						| 'view'
+		 * 						| 'edit'
+		 * 						| 'projects.add'
+		 * 						| 'projects.remove'
+		 * 						| 'projects.set'
+		 * 						| 'subscribers.add'
+		 * 						| 'subscribers.remove'
+		 * 						| 'subscribers.set'
+		 * 						| 'subtype'
+		 * 						| 'commnet'
+		 * 						| 'mfa';
+		 * 					value: string | boolean;
+		 * 				}
+		 * 			])} transactions An array of transactions to apply. This can include any 
+		 * custom types also. 
+		 * @param {(number | string)} objectIdentifier ID or PHID of the task
+		 * @returns {Promise<object>}
+		 */
+		edit: (
+			transactions: [
+				{
+					type:
+						| 'parent'
+						| 'column'
+						| 'space'
+						| 'title'
+						| 'owner'
+						| 'status'
+						| 'priority'
+						| 'description'
+						| 'parents.add'
+						| 'parents.remove'
+						| 'parents.set'
+						| 'subtasks.add'
+						| 'subtasks.remove'
+						| 'subtasks.set'
+						| 'commits.add'
+						| 'commits.remove'
+						| 'commits.set'
+						| 'view'
+						| 'edit'
+						| 'projects.add'
+						| 'projects.remove'
+						| 'projects.set'
+						| 'subscribers.add'
+						| 'subscribers.remove'
+						| 'subscribers.set'
+						| 'subtype'
+						| 'commnet'
+						| 'mfa';
+					value: string | boolean;
+				}
+			],
+			objectIdentifier: number | string
+		): Promise<object> => {
+			return this.makeRequest('maniphest.edit', {
+				transactions: transactions,
+				objectIdentifier: objectIdentifier
+			});
 		},
 
-		gettasktransactions: () => {
-			// TODO
+		/**
+		 ***Marked for deprecation** Retrieve Maniphest task transactions. 
+		 [Docs]{@link https://secure.phabricator.com/conduit/method/maniphest.gettasktransactions/}
+		 *
+		 * @param {Array<number>} ids An array of IDs 
+		 * @returns {Promise<object>}
+		 */
+		gettasktransactions: (ids: Array<number>): Promise<object> => {
+			return this.makeRequest('maniphest.gettasktransactions', { ids: ids });
 		},
 
 		/**
@@ -480,8 +614,14 @@ export class Condoit {
 			return this.makeRequest('maniphest.info', { task_id: taskId });
 		},
 
-		priority: () => {
-			// TODO
+		/**
+		 *Returns information about the possible priorities for Maniphest tasks. 
+		 [Docs]{@link https://secure.phabricator.com/conduit/method/maniphest.priority.search/}
+		 *
+		 * @returns {Promise<object>}
+		 */
+		prioritySearch: (): Promise<object> => {
+			return this.makeRequest('maniphest.priority.search', {});
 		},
 
 		/**
@@ -489,38 +629,37 @@ export class Condoit {
 		 [Docs]{@link https://secure.phabricator.com/conduit/method/user.whoami/}
 		 *
 		 * @param {({
-		 * 		ids: Array<number>;
-		 * 		phids: Array<string>;
-		 * 		ownerPHIDs: Array<string>;
-		 * 		authorPHIDs: Array<string>;
-		 * 		projectPHIDs: Array<string>;
-		 * 		ccPHIDs: Array<string>;
-		 * 		fullText: string;
-		 * 		status:
-		 * 			| 'status-any'
-		 * 			| 'status-open'
-		 * 			| 'status-closed'
-		 * 			| 'status-resolved'
-		 * 			| 'status-wontfix'
-		 * 			| 'status-invalid'
-		 * 			| 'status-spite'
-		 * 			| 'status-duplicate';
-		 * 		order: 'order-priority' | 'order-created' | 'order-modified';
-		 * 		limit: number;
-		 * 		offset: number;
-		 * 	})} options All options are optional
+		 * 			ids?: Array<number>;
+		 * 			phids?: Array<string>;
+		 * 			ownerPHIDs?: Array<string>;
+		 * 			authorPHIDs?: Array<string>;
+		 * 			projectPHIDs?: Array<string>;
+		 * 			ccPHIDs?: Array<string>;
+		 * 			fullText?: string;
+		 * 			status?:
+		 * 				| 'status-any'
+		 * 				| 'status-open'
+		 * 				| 'status-closed'
+		 * 				| 'status-resolved'
+		 * 				| 'status-wontfix'
+		 * 				| 'status-invalid'
+		 * 				| 'status-spite'
+		 * 				| 'status-duplicate';
+		 * 			order?: 'order-priority' | 'order-created' | 'order-modified';
+		 * 			limit?: number;
+		 * 			offset?: number;
+		 * 		})} options
 		 * @returns {Promise<object>}
-		 * @memberof Condoit
 		 */
 		query: (options: {
-			ids: Array<number>;
-			phids: Array<string>;
-			ownerPHIDs: Array<string>;
-			authorPHIDs: Array<string>;
-			projectPHIDs: Array<string>;
-			ccPHIDs: Array<string>;
-			fullText: string;
-			status:
+			ids?: Array<number>;
+			phids?: Array<string>;
+			ownerPHIDs?: Array<string>;
+			authorPHIDs?: Array<string>;
+			projectPHIDs?: Array<string>;
+			ccPHIDs?: Array<string>;
+			fullText?: string;
+			status?:
 				| 'status-any'
 				| 'status-open'
 				| 'status-closed'
@@ -529,9 +668,9 @@ export class Condoit {
 				| 'status-invalid'
 				| 'status-spite'
 				| 'status-duplicate';
-			order: 'order-priority' | 'order-created' | 'order-modified';
-			limit: number;
-			offset: number;
+			order?: 'order-priority' | 'order-created' | 'order-modified';
+			limit?: number;
+			offset?: number;
 		}): Promise<object> => {
 			return this.makeRequest('maniphest.query', {
 				ids: options?.ids,
@@ -560,89 +699,92 @@ export class Condoit {
 		},
 
 		/**
-		 *Search Maniphest tasks
+		 **Search Maniphest tasks
 		 [Docs]{@link https://secure.phabricator.com/conduit/method/maniphest.search/}
 		 *
 		 * @param {({
-		 * 		queryKey: 'assigned' | 'authored' | 'subscribed' | 'open' | 'all';
-		 * 		constraints: {
-		 * 			ids: Array<number>;
-		 * 			phids: Array<string>;
-		 * 			assigned: Array<string>;
-		 * 			authorPHIDs: Array<string>;
-		 * 			statuses: [
-		 * 				'open' | 'resolved' | 'wontfix' | 'invalid' | 'duplicate' | 'spite'
-		 * 			];
-		 * 			priorities: Array<number>;
-		 * 			subtypes: Array<string>;
-		 * 			columnPHIDs: Array<string>;
-		 * 			hasParents: boolean;
-		 * 			hasSubtasks: boolean;
-		 * 			parentIDs: Array<number>;
-		 * 			subtaskIDs: Array<number>;
-		 * 			createdStart: number;
-		 * 			createdEnd: number;
-		 * 			modifiedStart: number;
-		 * 			modifiedEnd: number;
-		 * 			closedStart: number;
-		 * 			closedEnd: number;
-		 * 			closerPHIDs: Array<string>;
-		 * 			query: string;
-		 * 			subscribers: Array<string>;
-		 * 			projects: Array<string>;
-		 * 		};
-		 * 		attachments: { columns: boolean; subscribers: boolean; projects: boolean };
-		 * 		order:
-		 * 			| 'priority'
-		 * 			| 'updated'
-		 * 			| 'outdated'
-		 * 			| 'newest'
-		 * 			| 'oldest'
-		 * 			| 'closed'
-		 * 			| 'title'
-		 * 			| 'relevance'
-		 * 			| [string];
-		 * 		before: string
-		 * 		after: string
-		 * 		limit: number
-		 * 	})} options All options are optional
+		 * 			queryKey?: 'assigned' | 'authored' | 'subscribed' | 'open' | 'all';
+		 * 			constraints?: {
+		 * 				ids?: Array<number>;
+		 * 				phids?: Array<string>;
+		 * 				assigned?: Array<string>;
+		 * 				authorPHIDs?: Array<string>;
+		 * 				statuses?: [
+		 * 					'open' | 'resolved' | 'wontfix' | 'invalid' | 'duplicate' | 'spite'
+		 * 				];
+		 * 				priorities?: Array<number>;
+		 * 				subtypes?: Array<string>;
+		 * 				columnPHIDs?: Array<string>;
+		 * 				hasParents?: boolean;
+		 * 				hasSubtasks?: boolean;
+		 * 				parentIDs?: Array<number>;
+		 * 				subtaskIDs?: Array<number>;
+		 * 				createdStart?: number;
+		 * 				createdEnd?: number;
+		 * 				modifiedStart?: number;
+		 * 				modifiedEnd?: number;
+		 * 				closedStart?: number;
+		 * 				closedEnd?: number;
+		 * 				closerPHIDs?: Array<string>;
+		 * 				query?: string;
+		 * 				subscribers?: Array<string>;
+		 * 				projects?: Array<string>;
+		 * 			};
+		 * 			attachments?: {
+		 * 				columns?: boolean;
+		 * 				subscribers?: boolean;
+		 * 				projects?: boolean;
+		 * 			};
+		 * 			order?:
+		 * 				| 'priority'
+		 * 				| 'updated'
+		 * 				| 'outdated'
+		 * 				| 'newest'
+		 * 				| 'oldest'
+		 * 				| 'closed'
+		 * 				| 'title'
+		 * 				| 'relevance'
+		 * 				| [string];
+		 * 			before?: string;
+		 * 			after?: string;
+		 * 			limit?: number;
+		 * 		})} options
 		 * @returns {Promise<object>}
-		 * @memberof Condoit
 		 */
 		search: (options: {
-			queryKey: 'assigned' | 'authored' | 'subscribed' | 'open' | 'all';
-			constraints: {
-				ids: Array<number>;
-				phids: Array<string>;
-				assigned: Array<string>;
-				authorPHIDs: Array<string>;
-				statuses: [
+			queryKey?: 'assigned' | 'authored' | 'subscribed' | 'open' | 'all';
+			constraints?: {
+				ids?: Array<number>;
+				phids?: Array<string>;
+				assigned?: Array<string>;
+				authorPHIDs?: Array<string>;
+				statuses?: [
 					'open' | 'resolved' | 'wontfix' | 'invalid' | 'duplicate' | 'spite'
 				];
-				priorities: Array<number>;
-				subtypes: Array<string>;
-				columnPHIDs: Array<string>;
-				hasParents: boolean;
-				hasSubtasks: boolean;
-				parentIDs: Array<number>;
-				subtaskIDs: Array<number>;
-				createdStart: number;
-				createdEnd: number;
-				modifiedStart: number;
-				modifiedEnd: number;
-				closedStart: number;
-				closedEnd: number;
-				closerPHIDs: Array<string>;
-				query: string;
-				subscribers: Array<string>;
-				projects: Array<string>;
+				priorities?: Array<number>;
+				subtypes?: Array<string>;
+				columnPHIDs?: Array<string>;
+				hasParents?: boolean;
+				hasSubtasks?: boolean;
+				parentIDs?: Array<number>;
+				subtaskIDs?: Array<number>;
+				createdStart?: number;
+				createdEnd?: number;
+				modifiedStart?: number;
+				modifiedEnd?: number;
+				closedStart?: number;
+				closedEnd?: number;
+				closerPHIDs?: Array<string>;
+				query?: string;
+				subscribers?: Array<string>;
+				projects?: Array<string>;
 			};
-			attachments: {
-				columns: boolean;
-				subscribers: boolean;
-				projects: boolean;
+			attachments?: {
+				columns?: boolean;
+				subscribers?: boolean;
+				projects?: boolean;
 			};
-			order:
+			order?:
 				| 'priority'
 				| 'updated'
 				| 'outdated'
@@ -652,9 +794,9 @@ export class Condoit {
 				| 'title'
 				| 'relevance'
 				| [string];
-			before: string;
-			after: string;
-			limit: number;
+			before?: string;
+			after?: string;
+			limit?: number;
 		}): Promise<object> => {
 			return this.makeRequest('maniphest.search', {
 				queryKey: options?.queryKey,
@@ -678,12 +820,57 @@ export class Condoit {
 			return this.makeRequest('maniphest.status.search', {});
 		},
 
-		status: () => {
-			// TODO
-		},
-
-		update: () => {
-			// TODO
+		/**
+		 ***Marked for deprecation** Update an existing Maniphest task. 
+		 [Docs]{@link https://secure.phabricator.com/conduit/method/maniphest.update/}
+		 *
+		 * @param {{
+		 * 			id?: number;
+		 * 			phid?: string;
+		 * 			title?: string;
+		 * 			description?: string;
+		 * 			ownerPHID?: string;
+		 * 			viewPolicy?: string;
+		 * 			editPolicy?: string;
+		 * 			ccPHIDs?: Array<string>;
+		 * 			priority?: number;
+		 * 			projectPHIDs?: Array<string>;
+		 * 			auxiliary?: object;
+		 * 			status?: string;
+		 * 			comments?: string;
+		 * 		}} options
+		 * @returns {Promise<object>}
+		 */
+		update: (options: {
+			id?: number;
+			phid?: string;
+			title?: string;
+			description?: string;
+			ownerPHID?: string;
+			viewPolicy?: string;
+			editPolicy?: string;
+			ccPHIDs?: Array<string>;
+			priority?: number;
+			projectPHIDs?: Array<string>;
+			auxiliary?: object;
+			status?: string;
+			comments?: string;
+		}): Promise<object> => {
+			return this.makeRequest('maniphest.update', {
+				id: options?.id,
+				phid: options?.phid,
+				title: options?.title,
+				description: options?.description,
+				ownerPHID: options?.ownerPHID,
+				viewPolicy: options?.viewPolicy,
+				editPolicy: options?.editPolicy,
+				ccPHIDs: options?.ccPHIDs,
+				priority: options?.priority,
+				projectPHIDs: options?.projectPHIDs,
+				auxiliary: options?.auxiliary,
+				status: options?.status,
+				comments: options?.comments
+			});
 		}
 	};
 
@@ -886,54 +1073,59 @@ export class Condoit {
 		 [Docs]{@link https://secure.phabricator.com/conduit/method/user.search/}
 		 *
 		 * @param {({
-		 * 		queryKey: 'active' | 'all' | 'approval';
-		 * 		constraints: {
-		 * 			ids: Array<number>;
-		 * 			phids: Array<string>;
-		 * 			usernames: Array<string>;
-		 * 			nameLike: string;
-		 * 			isAdmin: boolean;
-		 * 			isDisabled: boolean;
-		 * 			isBot: boolean;
-		 * 			isMailingList: boolean;
-		 * 			needsApproval: boolean;
-		 * 			mfa: boolean;
-		 * 			createdStart: number;
-		 * 			createdEnd: number;
-		 * 			query: string;
-		 * 		};
-		 * 		attachments: { availability: boolean };
-		 * 		order:
-		 * 			| 'newest'
-		 * 			| 'oldest'
-		 * 			| 'relevance'
-		 * 			| ['id' | 'username' | 'rank' | 'fulltext-created' | 'fulltext-modified'];
-		 * 		before: string;
-		 * 		after: string;
-		 * 		limit: number;
-		 * 	})} options All options are optional
+		 * 			queryKey?: 'active' | 'all' | 'approval';
+		 * 			constraints?: {
+		 * 				ids?: Array<number>;
+		 * 				phids?: Array<string>;
+		 * 				usernames?: Array<string>;
+		 * 				nameLike?: string;
+		 * 				isAdmin?: boolean;
+		 * 				isDisabled?: boolean;
+		 * 				isBot?: boolean;
+		 * 				isMailingList?: boolean;
+		 * 				needsApproval?: boolean;
+		 * 				mfa?: boolean;
+		 * 				createdStart?: number;
+		 * 				createdEnd?: number;
+		 * 				query?: string;
+		 * 			};
+		 * 			attachments?: { availability: boolean };
+		 * 			order?:
+		 * 				| 'newest'
+		 * 				| 'oldest'
+		 * 				| 'relevance'
+		 * 				| [
+		 * 						| 'id'
+		 * 						| 'username'
+		 * 						| 'rank'
+		 * 						| 'fulltext-created'
+		 * 						| 'fulltext-modified'
+		 * 				  ];
+		 * 			before?: string;
+		 * 			after?: string;
+		 * 			limit?: number;
+		 * 		})} options
 		 * @returns {Promise<object>}
-		 * @memberof Condoit
 		 */
 		search: (options: {
-			queryKey: 'active' | 'all' | 'approval';
-			constraints: {
-				ids: Array<number>;
-				phids: Array<string>;
-				usernames: Array<string>;
-				nameLike: string;
-				isAdmin: boolean;
-				isDisabled: boolean;
-				isBot: boolean;
-				isMailingList: boolean;
-				needsApproval: boolean;
-				mfa: boolean;
-				createdStart: number;
-				createdEnd: number;
-				query: string;
+			queryKey?: 'active' | 'all' | 'approval';
+			constraints?: {
+				ids?: Array<number>;
+				phids?: Array<string>;
+				usernames?: Array<string>;
+				nameLike?: string;
+				isAdmin?: boolean;
+				isDisabled?: boolean;
+				isBot?: boolean;
+				isMailingList?: boolean;
+				needsApproval?: boolean;
+				mfa?: boolean;
+				createdStart?: number;
+				createdEnd?: number;
+				query?: string;
 			};
-			attachments: { availability: boolean };
-			order:
+			attachments?: { availability: boolean };
+			order?:
 				| 'newest'
 				| 'oldest'
 				| 'relevance'
@@ -944,9 +1136,9 @@ export class Condoit {
 						| 'fulltext-created'
 						| 'fulltext-modified'
 				  ];
-			before: string;
-			after: string;
-			limit: number;
+			before?: string;
+			after?: string;
+			limit?: number;
 		}): Promise<object> => {
 			return this.makeRequest('user.search', {
 				queryKey: options?.queryKey,
